36d31fef60f6dd8ef8198dfd4df9340e6b825350
Vedanshankar <noahallen0603@gmail.com>
2026-08-22 12:45:37 +0530

    Add Spring Boot foundation

---
+++ src/main/java/com/krishinirnay/KrishinirnayApplication.java
@@ -0,0 +1,13 @@
+package com.krishinirnay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrishinirnayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KrishinirnayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/krishinirnay/KrishinirnayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krishinirnay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrishinirnayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
